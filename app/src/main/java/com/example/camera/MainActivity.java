9f2b12c1e7c91af150ac71a96e95c303ba87b88e
ubedjunejo <44271144+ubedjunejo@users.noreply.github.com>
2020-08-21 18:21:12 +0200

    Project changes

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -11,6 +11,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
 import android.graphics.Point;
@@ -25,6 +26,7 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.ImageReader;
@@ -35,10 +37,10 @@ import android.os.HandlerThread;
 import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -49,8 +51,10 @@ import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-//Camera part based on: https://androidpedia.net/en/tutorial/619/camera-2-api
-@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//Camera part based on: https://androidpedia.net/en/tutorial/619/camera-2-api and
+// https://github.com/googlearchive/android-Camera2Basic
+
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final int MAX_PREVIEW_WIDTH = 1920;
@@ -58,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private String mCameraId;
     private TextureView mTextureView;
     static TextureView  mlineView;
-    static SurfaceHolder mlineHolder;
     private CameraCaptureSession mCaptureSession;
     private CameraDevice mCameraDevice;
     private Size mPreviewSize;
@@ -70,18 +73,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private CaptureRequest mPreviewRequest;
     private Semaphore mCameraOpenCloseLock = new Semaphore(1);
     private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private long t;
 
     private SensorManager sensorManager;
-
-    private float[] accelerometerReading = new float[3];
-    private float[] magnetometerReading = new float[3];
-
-    private final float[] rotationMatrix = new float[9];
-    private final float[] orientationAngles = new float[3];//radians.
 
     private static final String TAG="MainActivity";
 
     private DrawSurface customView;
+    CameraManager manager;
 
 
     @Override
@@ -91,12 +90,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mTextureView = findViewById(R.id.texture);//view for camera
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        mlineView = findViewById(R.id.lineView);//view for line, to be stacked on camera view
-       // mlineHolder=mlineView.getHolder();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        mlineView = findViewById(R.id.lineView);//view for plots, to be stacked on camera view
         customView =new DrawSurface(this);//call class to draw line view on top of camera view
-        //setContentView(customView);
+        addContentView(customView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 
-
+        manager= (CameraManager) getSystemService(Context.CAMERA_SERVICE);
     }
 
     @Override
@@ -111,20 +113,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         //camera part; (re)open camera at resume
         if (mTextureView!=null && mTextureView.isAvailable()) {
+
             openCamera(mTextureView.getWidth(), mTextureView.getHeight());
+
         } else if (mTextureView!=null && mTextureView.isAvailable()==false) {
             mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
 
-        //line part; register listeners for acceleration and magnetic field
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //line part; register listener for acceleration
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         if (accelerometer != null) {
             sensorManager.registerListener(this, accelerometer,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
-        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        if (magneticField != null) {
-            sensorManager.registerListener(this, magneticField,
                     SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
         }
     }
@@ -135,44 +134,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         stopBackgroundThread();
         super.onPause();
         sensorManager.unregisterListener(this);
+
     }
 
-    // Get readings from accelerometer and magnetometer, to calculate orientation
+    // Get reading from accelerometer
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            /*System.arraycopy(event.values, 0, accelerometerReading,
-                    0, accelerometerReading.length);*/
-            accelerometerReading=event.values.clone();
-
-        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            /*System.arraycopy(event.values, 0, magnetometerReading,
-                    0, magnetometerReading.length);*/
-            magnetometerReading=event.values.clone();
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            customView.setAcceleration(event.values[1]);
         }
-
-        updateOrientationAngles();//update orientationAngles vector after sensor changes
-        double y=1.57079+orientationAngles[1];//only interested in y; x and z have nothing to do with orientation relative to ground
-        //Log.d(TAG, "y "+y*57.2958);
-
-        customView.setAngle((float)y);//send angle to stacked view to update line accordingly
-    }
-
-    // Compute the three orientation angles based on the most recent readings from
-    // the device's accelerometer and magnetometer.
-    public void updateOrientationAngles() {
-        // Update rotation matrix, which is needed to update orientation angles.
-        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading);
-        // "rotationMatrix" now has up-to-date information.
-
-        SensorManager.getOrientation(rotationMatrix, orientationAngles);
-        // "orientationAngles" now has up-to-date information.
     }
 
 
 /*******************************************************************************************/
 
-@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+@RequiresApi(api = Build.VERSION_CODES.M)
 private void openCamera(int width, int height) {
     //check for permissions; request if not granted already
     if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -181,12 +157,14 @@ private void openCamera(int width, int height) {
     }
     setUpCameraOutputs(width, height);
     configureTransform(width, height);
-    CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+
     try {
         if (!mCameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
             throw new RuntimeException("Time out waiting to lock camera opening.");
         }
         manager.openCamera(mCameraId, mStateCallback, mBackgroundHandler);
+
     } catch (CameraAccessException e) {
         e.printStackTrace();
     } catch (InterruptedException e) {
@@ -220,13 +198,11 @@ private void openCamera(int width, int height) {
 
 
     private void setUpCameraOutputs(int width, int height) {
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             for (String cameraId : manager.getCameraIdList()) {
                 CameraCharacteristics characteristics
                         = manager.getCameraCharacteristics(cameraId);
 
-                // We don't use a front facing camera in this sample.
                 Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
                 if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
                     continue;
@@ -238,12 +214,11 @@ private void openCamera(int width, int height) {
                     continue;
                 }
 
-                // For still image captures, we use the largest available size.
                 Size largest = Collections.max(
                         Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
                         new CompareSizesByArea());
                 mImageReader = ImageReader.newInstance(largest.getWidth(), largest.getHeight(),
-                        ImageFormat.JPEG, /*maxImages*/2);
+                        ImageFormat.JPEG,2);
                 mImageReader.setOnImageAvailableListener(
                         null, mBackgroundHandler);
 
@@ -262,9 +237,6 @@ private void openCamera(int width, int height) {
                     maxPreviewHeight = MAX_PREVIEW_HEIGHT;
                 }
 
-                // Danger! Attempting to use too large a preview size could  exceed the camera
-                // bus' bandwidth limitation, resulting in gorgeous previews but the storage of
-                // garbage capture data.
                 mPreviewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class),
                         rotatedPreviewWidth, rotatedPreviewHeight, maxPreviewWidth,
                         maxPreviewHeight, largest);
@@ -275,51 +247,43 @@ private void openCamera(int width, int height) {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         } catch (NullPointerException e) {
-            // Currently an NPE is thrown when the Camera2API is used but not supported on the
-            // device this code runs.
             Toast.makeText(this, "Camera2 API not supported on this device", Toast.LENGTH_LONG).show();
         }
     }
 
 
     private void createCameraPreviewSession() {
+
         try {
             SurfaceTexture texture = mTextureView.getSurfaceTexture();
             assert texture != null;
 
-            // We configure the size of default buffer to be the size of camera preview we want.
             texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
 
-            // This is the output Surface we need to start preview.
             Surface surface = new Surface(texture);
 
-            // We set up a CaptureRequest.Builder with the output Surface.
             mPreviewRequestBuilder
                     = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
             mPreviewRequestBuilder.addTarget(surface);
 
-            // Here, we create a CameraCaptureSession for camera preview.
             mCameraDevice.createCaptureSession(Arrays.asList(surface, mImageReader.getSurface()),
                     new CameraCaptureSession.StateCallback() {
 
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                            // The camera is already closed
                             if (null == mCameraDevice) {
                                 return;
                             }
 
-                            // When the session is ready, we start displaying the preview.
                             mCaptureSession = cameraCaptureSession;
                             try {
-                                // Auto focus should be continuous for camera preview.
-                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+                                //start flash
+                                mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_TORCH);
 
-                                // Finally, we start displaying the camera preview.
                                 mPreviewRequest = mPreviewRequestBuilder.build();
-                                mCaptureSession.setRepeatingRequest(mPreviewRequest,
-                                        null, mBackgroundHandler);
+                                mCaptureSession.setRepeatingRequest(mPreviewRequest, null, null);
+
                             } catch (CameraAccessException e) {
                                 e.printStackTrace();
                             }
@@ -328,7 +292,6 @@ private void openCamera(int width, int height) {
                         @Override
                         public void onConfigureFailed(
                                 @NonNull CameraCaptureSession cameraCaptureSession) {
-                            //showToast("Failed");
                         }
                     }, null
             );
@@ -399,9 +362,7 @@ private void requestCameraPermission() {
     private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
                                           int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
 
-        // Collect the supported resolutions that are at least as big as the preview Surface
         List<Size> bigEnough = new ArrayList<>();
-        // Collect the supported resolutions that are smaller than the preview Surface
         List<Size> notBigEnough = new ArrayList<>();
         int w = aspectRatio.getWidth();
         int h = aspectRatio.getHeight();
@@ -417,8 +378,6 @@ private void requestCameraPermission() {
             }
         }
 
-        // Pick the smallest of those big enough. If there is no one big enough, pick the
-        // largest of those not big enough.
         if (bigEnough.size() > 0) {
             return Collections.min(bigEnough, new CompareSizesByArea());
         } else if (notBigEnough.size() > 0) {
@@ -454,7 +413,7 @@ private void requestCameraPermission() {
     }
 
 /********************************************************************************************/
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private final TextureView.SurfaceTextureListener mSurfaceTextureListener
             = new TextureView.SurfaceTextureListener() {
 
@@ -476,11 +435,32 @@ private void requestCameraPermission() {
 
         @Override
         public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+            //based on: https://github.com/shivaneej/HeartRate
+            Bitmap bmp = mTextureView.getBitmap();
+            int width = bmp.getWidth();
+            int height = bmp.getHeight();
+            int[] pixels = new int[height * width];
+
+            // Get pixels from the bitmap, starting at (x,y) = (width/2,height/2)
+            // and totaling width/20 rows and height/20 columns
+            bmp.getPixels(pixels, 0, width, width / 2, height / 2, width / 20, height / 20);
+            int sum = 0;
+            for (int i = 0; i < height * width; i++) {
+                int red = (pixels[i] >> 16) & 0xFF;
+                sum = sum + red;//add up intensity of each pixel
+            }
+            t++;
+            if (t%4==0){//decrease update frequency to match that of accelerometer
+                double val=(sum-1200000)/1000;//magnify the difference by subtracting large base value; then divide to fit screen
+                customView.setBeat(val);
+            }
+
         }
 
     };
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
 
         @Override
@@ -489,6 +469,8 @@ private void requestCameraPermission() {
             mCameraOpenCloseLock.release();
             mCameraDevice = cameraDevice;
             createCameraPreviewSession();
+
+
         }
 
         @Override
@@ -511,10 +493,9 @@ private void requestCameraPermission() {
 
     static class CompareSizesByArea implements Comparator<Size> {
 
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
         public int compare(Size lhs, Size rhs) {
-            // We cast here to ensure the multiplications won't overflow
             return Long.signum((long) lhs.getWidth() * lhs.getHeight() - (long) rhs.getWidth() * rhs.getHeight());
         }
     }
